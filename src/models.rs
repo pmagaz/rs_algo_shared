0a1d9a3ac9530f40ce85549fc7bd19642a9fb4ba
Pablo <magazpablo@gmail.com>
2022-03-15 12:02:08 +0100

    Update models.rs

--- src/models.rs
+++ src/models.rs
@@ -243,13 +243,13 @@ pub enum CandleType {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Candle {
-    candle_type: CandleType,
-    date: DateTime<Local>,
-    open: f64,
-    high: f64,
-    low: f64,
-    close: f64,
-    volume: f64,
+    pub candle_type: CandleType,
+    pub date: DateTime<Local>,
+    pub open: f64,
+    pub high: f64,
+    pub low: f64,
+    pub close: f64,
+    pub volume: f64,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
