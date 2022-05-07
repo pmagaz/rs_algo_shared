e2f454ad42bd1920f7792ed97b77d785796054a3
Pablo <magazpablo@gmail.com>
2022-05-07 08:36:54 +0200

    feat: add to_dbtime

--- src/helpers/date.rs
+++ src/helpers/date.rs
@@ -6,3 +6,7 @@ pub fn parse_time(date: i64) -> DateTime<Local> {
     let ts = chrono::NaiveDateTime::from_timestamp(date, 0);
     Local.from_utc_datetime(&ts)
 }
+
+pub fn to_dbtime(date: DateTime<Local>) -> DbDateTime {
+    DbDateTime::from_chrono(date + Duration::hours(2))
+}
