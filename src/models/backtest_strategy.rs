a273fe05a6b828f8f3e2da4dc25f87c8808876bf
Pablo <magazpablo@gmail.com>
2022-05-14 08:32:23 +0200

    feat: add BackTestStrategyResult model

---
+++ src/models/backtest_strategy.rs
@@ -0,0 +1,25 @@
+use crate::helpers::date::*;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BackTestStrategyResult {
+    pub avg_strategy: String,
+    pub avg_date_start: DbDateTime,
+    pub avg_date_end: DbDateTime,
+    pub avg_avg_sessions: usize,
+    pub avg_trades: usize,
+    pub avg_wining_trades: usize,
+    pub avg_losing_trades: usize,
+    pub avg_stop_losses: usize,
+    pub avg_gross_profit: f64,
+    pub avg_commissions: f64,
+    pub avg_net_profit: f64,
+    pub avg_net_profit_per: f64,
+    pub avg_profitable_trades: f64,
+    pub avg_profit_factor: f64,
+    pub avg_max_runup: f64,
+    pub avg_max_drawdown: f64,
+    pub avg_buy_hold: f64,
+    pub avg_annual_return: f64,
+}
