edde0b292fd05d02623bb3d8e5ce882403ececcf
Pablo <magazpablo@gmail.com>
2023-01-22 14:38:07 +0100

    feat: add pricing

---
+++ src/models/pricing.rs
@@ -0,0 +1,41 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Pricing {
+    symbol: String,
+    ask: f64,
+    bid: f64,
+    spread: f64,
+    pip_size: f64,
+}
+
+impl Pricing {
+    pub fn symbol(&self) -> String {
+        self.symbol.to_string()
+    }
+    pub fn ask(&self) -> f64 {
+        self.ask
+    }
+    pub fn bid(&self) -> f64 {
+        self.bid
+    }
+    pub fn spread(&self) -> f64 {
+        self.spread
+    }
+
+    pub fn pip_size(&self) -> f64 {
+        self.pip_size
+    }
+}
+
+impl Default for Pricing {
+    fn default() -> Self {
+        Pricing {
+            symbol: "".to_string(),
+            ask: 0.,
+            bid: 0.,
+            spread: 0.,
+            pip_size: 0.,
+        }
+    }
+}
