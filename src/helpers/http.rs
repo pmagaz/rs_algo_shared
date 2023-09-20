67a77ef76aff5261039d3b630c51eb2704241f7a
Pablo <magazpablo@gmail.com>
2023-09-20 14:30:25 +0200

    chore: add package size to http

--- src/helpers/http.rs
+++ src/helpers/http.rs
@@ -19,10 +19,18 @@ pub enum Format {
 
 pub async fn request<T>(url: &str, data: &T, method: HttpMethod) -> Result<Response, Error>
 where
-    for<'de> T: Serialize + Deserialize<'de> + Debug,
+    T: Serialize + Deserialize<'static> + Debug,
 {
-    println!("[HTTP] {:?} request to {}", method, url);
+    // Serialize the data to JSON and calculate the size of the request body
+    let request_body = serde_json::to_string(data).unwrap();
+    let request_size = request_body.len();
 
+    println!(
+        "[HTTP] {:?} request to {} with request size: {} bytes",
+        method, url, request_size
+    );
+
+    let client = Client::new();
     let result = match method {
         HttpMethod::Post => Client::builder().build()?.post(url),
         HttpMethod::Put => Client::builder().build()?.put(url),
@@ -30,7 +38,15 @@ where
         HttpMethod::Patch => Client::builder().build()?.patch(url),
         HttpMethod::Delete => Client::builder().build()?.delete(url),
     };
-    
 
-    result.json(&data).send().await
+    result
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .body(request_body)
+        .send()
+        .await
 }
+
+
+
+
+
